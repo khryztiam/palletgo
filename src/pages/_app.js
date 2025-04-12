bedad48217d6d72bbbfacfafb5153bbdd4140563
khryztiam <31896683+khryztiam@users.noreply.github.com>
2025-04-12 10:01:51 -0600

    Favicon y correccion responvise Boarding

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@
 //import AuthGate from '@/components/AuthGate';
 import { AuthProvider, useAuth } from '@/context/AuthContext';
 import { Navbar } from '@/components/Navbar';
+import Head from 'next/head';
 
 
 import '@/styles/global.css';
@@ -19,7 +20,22 @@ import '@/styles/management.css';
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Navbar />
+        <Head>
+          <title>PalletGo App</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="PalletGo WebApp"
+            content="Web site created for send request to boarding area from production"
+          />
+        </Head>
+        <Navbar />
       <Component {...pageProps} />
     </AuthProvider>
   );
